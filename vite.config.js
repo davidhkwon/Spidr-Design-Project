95a9f126f790728da22fb25c7a2e9feb36d8c516
David Kwon <david.h.kwon@outlook.com>
2025-07-16 19:23:05 -0500

    Attempt fix for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === production ? '/Spidr-Design-Project/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Spidr-Design-Project/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
